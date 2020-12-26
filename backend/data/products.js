50c511bb39c979e1e88999a39220644a1301a1f8
Philip Walsh <philipwalsh.ds@gmail.com>
2020-12-26 13:37:35 -0500

    add middleware error traps

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,6 @@
 const products = [
   {
-    name: 'Airpods Wireless Bluetooth Headphones',
+    name: 'Airpods Wireless Bluetoof Headphones',
     image: '/images/airpods.jpg',
     description:
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
-
-
-
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js'
+//import { restart } from 'nodemon'
 dotenv.config()
-
 connectDB()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 const CURRENT_ENV = process.env.NODE_ENV
+
+
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`server running on port ${PORT} in ${CURRENT_ENV} mode`.green))
 
@@ -20,12 +25,5 @@ app.get("/", (req, res) => {
     res.send("api running...")
 })
 
-app.get("/api/products", (req, res) => {
-res.json(products)
-})
 
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find(p => p._id===req.params.id)
-    res.json(product)
-    })
     
