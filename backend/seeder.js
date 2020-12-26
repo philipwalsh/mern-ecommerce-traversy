d4b89ca907e7f26fc929db50e192b5b937135a53
Philip Walsh <philipwalsh.ds@gmail.com>
2020-12-26 12:20:08 -0500

    loaded sample data

---
+++ backend/seeder.js
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import users from './data/users.js'
+import products from './data/products.js'
+
+import User from './models/userModel.js'
+import Product from './models/productModel.js'
+import Order from './models/orderModel.js'
+import connectDB from './config/db.js'
+
+
+
+const importData = async() => {
+    try {
+        await Order.deleteMany()
+        await Product.deleteMany()
+        await User.deleteMany()
+
+        const createdUsers = await User.insertMany(users)
+        const adminUser = createdUsers[0]._id
+        const sampleProducts = products.map(product =>{
+            return {...product, user: adminUser}
+        })
+        await Product.insertMany(sampleProducts)
+        console.log('data imported'.green)
+
+    } catch (error) {
+        console.error(`${error}`.red)        
+    }
+}
+
+
+const destroyData = async() => {
+    try {
+        await Order.deleteMany()
+        await Product.deleteMany()
+        await User.deleteMany()
+
+        console.log('data destroyed'.green)
+
+    } catch (error) {
+        console.error(`${error}`.red)        
+    }
+}
+
+
+if(process.argv[2]==='-d' || process.argv[2]==='-i' ){
+    dotenv.config()
+    connectDB()
+}
+
+if(process.argv[2]==='-d'){
+    destroyData()
+}else{
+    if(process.argv[2]==='-i'){
+        importData() 
+    }else{
+        console.log('need a switch either d[elete] or -i[nsert]')
+    }
+
+}
