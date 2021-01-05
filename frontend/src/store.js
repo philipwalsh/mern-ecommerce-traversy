4b22fe966ccf2e22b12d55012d9eeba3f9039d42
Philip Walsh <philipwalsh.ds@gmail.com>
2021-01-05 15:51:52 -0500

    turle progress

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools} from 'redux-devtools-extension'
+import {productListReducer} from './reducers/productReducers'
 
-
-const reducers = combineReducers({})
+const reducers = combineReducers({
+    productList: productListReducer
+})
 const initialState = {}
 const middleware = [thunk]
 const store = createStore( reducers, initialState, composeWithDevTools(applyMiddleware(...middleware)))
